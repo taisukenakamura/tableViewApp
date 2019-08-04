c461e9cdc5e7f7d99a5cf000c7b713599f837caa
中村泰輔 <nakamurayasusuke@MacBook-Pro.local>
2019-08-04 17:46:31 +0900

    tableView作成

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tableViewApp
+//
+//  Created by 中村泰輔 on 2019/08/04.
+//  Copyright © 2019 icannot.t.n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
